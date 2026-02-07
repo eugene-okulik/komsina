f49ace7a1f1f67c07a4167dfd9c0db3c05eb3a29
Tanya Stulova <komsina.ts@gmail.com>
2026-02-07 23:03:40 +0300

    homework_6 done

---
+++ homework/komsina_tati/homework_6/task_1.py
@@ -0,0 +1,16 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+words = text.split()
+fin_words = []
+for word in words:
+    if word.endswith(','):
+        word = word[:-1]
+        fin_word = word + 'ing' + ','
+        fin_words.append(fin_word)
+    elif word.endswith('.'):
+        word = word[:-1]
+        fin_word = word + 'ing' + '.'
+        fin_words.append(fin_word)
+    else:
+        fin_word = word + 'ing'
+        fin_words.append(fin_word)
+print(' '.join(fin_words))
